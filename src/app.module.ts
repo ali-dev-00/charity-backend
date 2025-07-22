227e483b576837f7949aa7ac7e4096c5707aaaac
Ali Dev <mirzaaly8@gmail.com>
2025-07-22 11:00:00 +0500

    intial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
-import { AlumniModule } from './alumnimembers/alumni.module';
+import { AlumniModule } from './alumni-registration/alumni.module';
 import { AuthModule } from './auth/auth.module';
 import { VolunteerModule } from './volunteer-registration/volunteer.module';
 
